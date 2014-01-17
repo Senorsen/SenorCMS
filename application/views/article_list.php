950a20f7e363a3440346e085fb4c578dd6a70f05
Senorsen <zhs490770@foxmail.com>
2014-01-17 21:02:38 +0800

    update noticication text

--- application/views/article_list.php
+++ application/views/article_list.php
@@ -1,4 +1,4 @@
-<script>alert('未完成，进度缓慢，可能会在寒假结束前完成此网站制作，手机版在电脑版之后。目前暂时使用wordpress博客，http://blog.senorsen.com ~谢谢光临');</script>
+<script>alert('未完成，进度缓慢，可能会在寒假结束前完成此网站制作，手机版在电脑版之后。\n可能考虑放弃osulist.js\n目前暂时使用wordpress博客，http://blog.senorsen.com ~谢谢光临');</script>
     <div id="top-layer">
         
     </div>
